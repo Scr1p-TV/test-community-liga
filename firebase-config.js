470086fea71c81ba6ebcef167a2161e9bc10e2ba
Scr1p-TV <dirk89.dh@gmail.com>
2026-05-10 18:22:15 +0200

    Update Firebase configuration with new project details

--- firebase-config.js
+++ firebase-config.js
@@ -2,11 +2,12 @@
 // Diese Daten bekommst du aus Firebase Console
 
 export const firebaseConfig = {
-  apiKey: "DEIN_API_KEY",
-  authDomain: "DEIN_PROJEKT.firebaseapp.com",
-  databaseURL: "https://DEIN_PROJEKT-default-rtdb.firebaseio.com",
-  projectId: "DEIN_PROJEKT",
-  storageBucket: "DEIN_PROJEKT.appspot.com",
-  messagingSenderId: "123456",
-  appId: "APP_ID"
+  apiKey: "AIzaSyAXwz7M6dEqAocdjCrJA7nE46NIizPmQlk",
+  authDomain: "ufl-community-draft.firebaseapp.com",
+  databaseURL: "https://ufl-community-draft-default-rtdb.europe-west1.firebasedatabase.app/",
+  projectId: "ufl-community-draft",
+  storageBucket: "ufl-community-draft.firebasestorage.app",
+  messagingSenderId: "170060497638",
+  appId: "1:170060497638:web:226577a80f754ee09d7127",
+  measurementId: "G-SGRD52SBPM"
 };
